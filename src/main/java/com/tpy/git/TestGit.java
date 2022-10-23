d0b4f6f75d7f5f7ee36c73307f05fb7c47dfbd47
tpy <tpy0330@qq.com>
2022-10-23 17:56:07 +0800

    second commit

--- src/main/java/com/tpy/git/TestGit.java
+++ src/main/java/com/tpy/git/TestGit.java
@@ -3,5 +3,6 @@ package com.tpy.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello Git1!");
+        System.out.println("hello Git2!");
     }
 }
